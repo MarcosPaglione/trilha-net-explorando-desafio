bfe03d900e1a93c36534e5ab69b5bcedfdae41fe
Marcos Eliezer Paglione <paglione.technology@outlook.com>
2024-12-01 11:46:23 -0300

    Desafio concluidogit status!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -15,16 +17,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // OK: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // OK: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Capacidade da Suite é MENOR do que a quantidade de HOSPEDES");
             }
         }
 
@@ -35,23 +36,29 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // OK: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            int quant = Hospedes.Count; 
+            return quant;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
+            // OK: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            try
+            { 
+                valor = Suite.ValorDiaria * Convert.ToDecimal(DiasReservados); 
+            }
+            catch(Exception ex)
             {
-                valor = 0;
+                Console.WriteLine($"Nao foi possivel realizar a conversao {ex}");
+            }
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = valor * 0.1M; 
+                valor = valor - desconto;
             }
 
             return valor;
